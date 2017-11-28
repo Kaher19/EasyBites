1befb9a790b172039a8746fca288ee1caa99363d
kevinbasto <31493060+kevinbasto@users.noreply.github.com>
2017-11-28 17:01:51 -0600

    Actualizacion 001 28/11/2017
    Actualizacion que ajusta la clase EasyBites para incluirme como modificador de la clase

--- src/easybites/EasyBites.java
+++ src/easybites/EasyBites.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Kevin Daniel Basto Anquino
+ * @author Kirbey
  */
 package easybites;
 
-/**
- *
- * @author Kirbey
- */
 public class EasyBites {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
