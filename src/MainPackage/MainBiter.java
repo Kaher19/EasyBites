08a3dd27786d16bf3b039c55d3de2e7e1057a1dc
kevinbasto <31493060+kevinbasto@users.noreply.github.com>
2017-12-05 15:01:27 -0600

    el comienzo de un comienzo
    este es el comienzo, despues de dos intentos fallidos anteriormente

---
+++ src/MainPackage/MainBiter.java
@@ -0,0 +1,10 @@
+/**
+ * este proyecto pertenece al equipo 2k compuesto por
+ * @author Kevin Daniel Basto Anquino
+ * @author Kirbey Aldair García Hernandez
+ */
+package MainPackage;
+
+public class MainBiter {
+    
+}
