23c764dc92d11c9de6148fcbc10a582741214a81
kevinbasto <31493060+kevinbasto@users.noreply.github.com>
2017-12-05 15:16:04 -0600

    actualizacion
    inclusion de los constructores de cada una de las variables utilizadas dentro de la base de datos

---
+++ src/Model/Connection.java
@@ -0,0 +1,10 @@
+/**
+ *  esta clase fue creada con el motivo de iniciar la conexion del programa a la
+ *  base de datos del programa
+ * @author Kevin Daniel Basto Anquino
+ */
+package Model;
+
+public class Connection {
+    
+}
